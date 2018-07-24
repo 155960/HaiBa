25257cea63f18a908934149d6f2bf1d4caaf8d21
zengqiang <1559603546@qq.com>
2018-07-24 18:55:20 +0800

    temp

--- src/main/java/com/zengqiang/future/controller/FileController.java
+++ src/main/java/com/zengqiang/future/controller/FileController.java
@@ -12,7 +12,6 @@ public class FileController {
 
     @RequestMapping("upload")
     public ServerResponse upload(@RequestParam("files") MultipartFile[] files,int type){
-
         return null;
     }
 }
